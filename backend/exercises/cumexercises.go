17f1db0bb889fbbbecf657bd917d7759985291c5
tausiq2003 <tausiqsamantaray03@gmail.com>
2025-05-20 22:34:28 +0530

    first exercise done

--- backend/exercises/cumexercises.go
+++ backend/exercises/cumexercises.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		prevZ := z
+		z -= (z*z - x) / (2 * z)
+		if math.Abs(prevZ-z) < 1e-15 {
+			break
+		}
+		fmt.Println("Testing", z)
+	}
+	return z
+}
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println(Sqrt(10))
+
 }
